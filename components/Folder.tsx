8441affa6a7941c98f3c09ddb410ab456bb74664
fozzy2112 <difiorahmandha21@gmail.com>
2023-01-28 02:13:30 +0700

    test

---
+++ components/Folder.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Folder() {
+
+	return (
+		<input 
+			type="text" 
+			placeholder="Cari username Github" 
+			className="border border-zinc-100 p-[20px] rounded-[8px]" 
+		/>
+	)
+}
