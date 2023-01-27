e2a64a055bb58793d4d1fd9551894ba75ce146b6
fozzy2112 <difiorahmandha21@gmail.com>
2023-01-28 02:23:53 +0700

    fix: set tailwind css

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 
-export default function Form() {
+export default function Form({value,onChange}:Props) {
 
 	return (
 		<input 
 			type="text" 
 			placeholder="Cari username Github" 
 			className="border border-zinc-100 p-[20px] rounded-[8px]" 
+			value={value}
+			onChange={onChange}
 		/>
 	)
+}
+
+interface Props{
+	value?: string;
+	onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
